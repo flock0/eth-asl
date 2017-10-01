1ba17134101b33ee434731d7a0be723e191cc45a
Florian Chlan <florian@chlan.net>
2017-10-01 18:17:16 +0200

    Found better way to stop polling of clients with pending requests; Added exception handling to SocketsHandler

--- src/ch/ethz/asl/net/SocketsHandler.java
+++ src/ch/ethz/asl/net/SocketsHandler.java
@@ -1,6 +1,5 @@
 package ch.ethz.asl.net;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
@@ -14,6 +13,8 @@ import java.util.concurrent.BlockingQueue;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import ch.ethz.asl.RunMW;
 
 /***
  * Thread that handles all network connections
@@ -37,10 +38,10 @@ public class SocketsHandler implements Runnable {
 	final List<String> mcAddresses;
 	final int numThreadsPTP;
 	final boolean readSharded;
-	private static Selector selector = null;
+	private Selector selector = null;
 	
 	
-	private BlockingQueue<SocketChannel> channelQueue = new ArrayBlockingQueue<SocketChannel>(MAX_CHANNEL_QUEUE_CAPACITY, false);
+	private BlockingQueue<SelectionKey> channelQueue = new ArrayBlockingQueue<SelectionKey>(MAX_CHANNEL_QUEUE_CAPACITY, false);
 	
 	private static final Logger logger = LogManager.getLogger(SocketsHandler.class);
 	
@@ -59,67 +60,81 @@ public class SocketsHandler implements Runnable {
 		 */
 		
 		logger.debug("Opening ServerSocket on {}:{}", myIp, myPort);
-		// Selector: multiplexor of SelectableChannel objects
-		selector = Selector.open(); // selector is open here
+		try {
+			// Selector: multiplexor of SelectableChannel objects
+			selector = Selector.open(); // selector is open here
 		
-		// ServerSocketChannel: selectable channel for stream-oriented listening sockets
-		ServerSocketChannel serverSocket = ServerSocketChannel.open();
-		InetSocketAddress serverAddr = new InetSocketAddress(myIp, myPort);
- 
-		// Binds the channel's socket to a local address and configures the socket to listen for connections
-		serverSocket.bind(serverAddr);
- 
-		// Adjusts this channel's blocking mode.
-		serverSocket.configureBlocking(false);
- 
-		int ops = serverSocket.validOps();
-		SelectionKey selectKy = serverSocket.register(selector, ops, null);
- 
-		logger.info("ServerSocket opened. Listening for incoming connections...");
+			// ServerSocketChannel: selectable channel for stream-oriented listening sockets
+			ServerSocketChannel serverSocket = ServerSocketChannel.open();
+			InetSocketAddress serverAddr = new InetSocketAddress(myIp, myPort);
+	 
+			// Binds the channel's socket to a local address and configures the socket to listen for connections
+			serverSocket.bind(serverAddr);
+	 
+			// Adjusts this channel's blocking mode.
+			serverSocket.configureBlocking(false);
+	 
+			int ops = serverSocket.validOps();
+			serverSocket.register(selector, ops, null);
 
-		// Infinite loop..
-		// Keep server running
-		while (true) {
- 
-			// Selects a set of keys whose corresponding channels are ready for I/O operations
-			selector.select();
-			
-			// token representing the registration of a SelectableChannel with a Selector
-			Set<SelectionKey> keys = selector.selectedKeys();
-			Iterator<SelectionKey> iter = keys.iterator();
- 
-			while (iter.hasNext()) {
-				SelectionKey key = iter.next();
- 
-				// Tests whether this key's channel is ready to accept a new socket connection
-				if (key.isValid() && key.isAcceptable()) {
-					SocketChannel client = serverSocket.accept();
- 
-					// Adjusts this channel's blocking mode to false
-					client.configureBlocking(false);
- 
-					// Operation-set bit for read operations
-					client.register(selector, SelectionKey.OP_READ);
-					logger.debug("New connection accepted: " + client.getLocalAddress());
- 
-					// Tests whether this key's channel is ready for reading
-				} else if (key.isValid() && key.isReadable()) {
-					
-					// Here we only figured out whether a client has data to read.
-					// The parsing of the message will not be done in the network thread.
-					// We will do this in the workers.
-					
-					// Stop polling on this client connection while it's waiting 
-					// in the queue and is being processed.
-					key.cancel(); 
-					enqueueChannel(key);
-					
+			logger.info("ServerSocket opened. Listening for incoming connections...");
+	
+			// Infinite loop..
+			// Keep server running
+			while (true) {
+	 
+				// Selects a set of keys whose corresponding channels are ready for I/O operations
+				selector.select();
+				
+				// token representing the registration of a SelectableChannel with a Selector
+				Set<SelectionKey> keys = selector.selectedKeys();
+				Iterator<SelectionKey> iter = keys.iterator();
+	 
+				while (iter.hasNext()) {
+					SelectionKey key = iter.next();
+	 
+					// Tests whether this key's channel is ready to accept a new socket connection
+					if (key.isValid() && key.isAcceptable()) {
+						SocketChannel client = serverSocket.accept();
+	 
+						try {
+							// Adjusts this channel's blocking mode to false
+							client.configureBlocking(false);
+		 
+							// Operation-set bit for read operations
+							client.register(selector, SelectionKey.OP_READ);
+							logger.debug("New connection accepted: " + client.getLocalAddress());
+						} catch(Exception ex) {
+							// Exception occured in a specific server socket.
+							// Close it and continue!
+							logger.catching(ex);
+							client.close();
+						}
+	 
+						// Tests whether this key's channel is ready for reading
+					} else if (key.isValid() && key.isReadable()) {
 						
-					
-					
+						// Here we only figured out whether a client has data to read.
+						// The parsing of the message will not be done in the network thread.
+						// We will do this in the workers.
+						
+						// Stop polling on this client connection while it's waiting 
+						// in the queue and is being processed.
+						key.cancel(); 
+						enqueueChannel(key);
+						
+							
+						
+						
+					}
+					iter.remove();
 				}
-				iter.remove();
 			}
+		} catch (Exception ex) {
+			// An exception occurred in the network thread.
+			// We can't recover from that. Shutdown!
+			logger.catching(ex);
+			RunMW.shutdown();
 		}
 
 	}
@@ -129,16 +144,18 @@ public class SocketsHandler implements Runnable {
 	 * stops the selector from checking this channel.
 	 * Assume that the given channel is valid and has data to read
 	 * @param key A key to the underlying channel
-	 * @throws InterruptedException if the queue is at full capacity and if we are interrupted during waiting.
 	 */
-	private void enqueueChannel(SelectionKey key) throws InterruptedException {
-		channelQueue.put((SocketChannel)key.channel());
-		key.cancel();
+	private void enqueueChannel(SelectionKey key) {
+		try {
+			channelQueue.put(key);
+			key.interestOps(0); // Stop the selector from checking this channel temporarily while it's request is being handled.
+		} catch (InterruptedException ex) {
+			// We got interrupted while waiting for a space in the queue to become available.
+			// This could occur when shutting down the system. Nothing else to do here.
+			logger.catching(ex);
+		}
 		
-	}
-
-	public static Selector getSelector() {
-		return selector;
+		
 	}
 
 }

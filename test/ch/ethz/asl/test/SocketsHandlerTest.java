0964b87052ae1cc98ada36b6315dbc9df9c30eb2
Florian Chlan <florian@chlan.net>
2017-10-01 18:25:33 +0200

    Added JUnit to project

---
+++ test/ch/ethz/asl/test/SocketsHandlerTest.java
@@ -0,0 +1,29 @@
+package ch.ethz.asl.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SocketsHandlerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSocketsHandler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+}

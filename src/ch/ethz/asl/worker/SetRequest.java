ea18305cfa5b70c60078def1919b50c7c55111a2
Florian Chlan <florian@chlan.net>
2017-10-09 15:25:32 +0200

    Added scaffolding for SetRequest implementation, WIP

--- src/ch/ethz/asl/worker/SetRequest.java
+++ src/ch/ethz/asl/worker/SetRequest.java
@@ -1,14 +1,26 @@
 package ch.ethz.asl.worker;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import ch.ethz.asl.net.MemcachedSocketHandler;
 
 public class SetRequest implements Request {
 
+	private static final Logger logger = LogManager.getLogger(SetRequest.class);
+	
 	byte[] command;
+	ByteBuffer commandBuffer;
+	int commandLength;
+	
 	public SetRequest(byte[] command) {
 		this.command = command;
+		this.commandBuffer = ByteBuffer.wrap(command);
 	}
 
 	@Override
@@ -17,9 +29,46 @@ public class SetRequest implements Request {
 	}
 
 	@Override
-	public void handle(MemcachedSocketHandler memcachedSocketHandler, ByteBuffer clientBuff) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not yet implemented");
+	public void handle(MemcachedSocketHandler memcachedSocketHandler, SocketChannel client, ByteBuffer clientBuff) {
+		
+		
+		//TODO Forward set command to all memcached servers
+		memcachedSocketHandler.sendToAll(commandBuffer);
+		for(SocketChannel serverChannel : memcachedSocketHandler.getChannels()) {
+			do {
+				serverChannel.write(commandBuffer);
+			} while(commandBuffer.hasRemaining());
+			
+			commandBuffer.position(0); // We send the same message to all memcached servers, hence a simple reset of the position.
+		}
+		
+		//TODO Wait for responses of all memcached servers
+		List<String> responses = memcachedSocketHandler.waitForAllResponses();
+		for(SocketChannel serverChannel : memcachedSocketHandler.getChannels()) {
+		
+		}
+
+		List<String> errors = getErrors(responses);
+		if(errors.isEmpty())
+			sendSuccessToClient(client, clientBuff);
+		else
+			sendSingleErrorMessage(errors);
+			
+		
+		// TODO Auto-generated method stub. Remove code below
+		logger.debug("set request");
+		String answer = "STORED\r\n";
+		byte[] answerBytes = answer.getBytes();
+		buff.put(answerBytes);
+		buff.flip();
+		int sentBytes = 0;
+		try {
+			do {	
+					sentBytes += client.write(buff);	
+			} while(sentBytes < answerBytes.length);
+		} catch (IOException ex) {
+			logger.catching(ex);
+		}
 	}
 
 }

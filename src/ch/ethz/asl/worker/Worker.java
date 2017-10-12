25f1f4f195fc90a5cdb0341dcd6e4d7c4a122407
Florian Chlan <florian@chlan.net>
2017-10-12 17:05:42 +0200

    Got rid of obsolete code in Worker

--- src/ch/ethz/asl/worker/Worker.java
+++ src/ch/ethz/asl/worker/Worker.java
@@ -13,9 +13,8 @@ import ch.ethz.asl.net.SocketsHandler;
 
 public class Worker implements Runnable {
 	
-	static final int CLIENT_BUFFER_MAX_BYTES_SIZE = 3000; //Maximum size of a get command is 2513
-    private SelectionKey key;
-	private SocketsHandler socketsHandler;
+    private SocketChannel client;
+	private Request request;
     
 	private static final Logger logger = LogManager.getLogger(Worker.class);
 	
@@ -28,56 +27,15 @@ public class Worker implements Runnable {
         
         
     };
-         
-	private static final ThreadLocal<ByteBuffer> clientBuffer = 
-	         new ThreadLocal<ByteBuffer>(){
-	      @Override
-	      protected ByteBuffer initialValue(){
-	          return ByteBuffer.allocate(CLIENT_BUFFER_MAX_BYTES_SIZE);
-	      }
-	      
-	      
-	};
+        	
 
-    public Worker(SelectionKey key, SocketsHandler socketsHandler){              
-        this.key = key;
-        this.socketsHandler = socketsHandler;
+    public Worker(SocketChannel client, Request req){              
+        this.client = client;
+        this.request = req;
     }
 
     public void run(){
-    	logger.debug("Processing request on worker thread");
-    	ByteBuffer clientBuff = clientBuffer.get();
-    	SocketChannel client = (SocketChannel)key.channel();
-    	
-    	int readReturnCode = -2; //Initially set to some unused code
-    	do {
-    		try {
-				readReturnCode = client.read(clientBuff);
-			} catch (IOException ex) {
-				logger.catching(ex);
-			}
-    	} while(readReturnCode != 0 && readReturnCode != -1); //TODO Handle overflow of buffer
-    	
-		if(readReturnCode == -1)
-			try {
-				client.close();
-			} catch (IOException ex) {
-				logger.catching(ex);
-			}
-		else {
-			clientBuff.flip();
-			try {
-				Request req = RequestFactory.createRequest(clientBuff);
-				clientBuff.clear();
-				req.handle(sockets.get(), client, clientBuff);
-			} catch (RequestParsingException ex) {
-				//Couldn't parse 
-				logger.catching(ex);
-			} finally {
-				clientBuff.clear();
-				key.interestOps(SelectionKey.OP_READ);
-				socketsHandler.wakeupSelector();
-			}
-		}
+    	logger.debug(String.format("Processing %s on worker thread", request));
+    	request.handle(sockets.get(), client);
     }
 }

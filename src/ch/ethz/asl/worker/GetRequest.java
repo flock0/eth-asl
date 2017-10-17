4a9acc929e4e8c04acbcb269c452a643ace183e5
Florian Chlan <florian@chlan.net>
2017-10-17 16:30:34 +0200

    GetRequest: Fixed handling of connection drops

--- src/ch/ethz/asl/worker/GetRequest.java
+++ src/ch/ethz/asl/worker/GetRequest.java
@@ -7,6 +7,7 @@ import java.nio.channels.SocketChannel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import ch.ethz.asl.RunMW;
 import ch.ethz.asl.net.MemcachedSocketHandler;
 
 public class GetRequest implements Request {
@@ -38,16 +39,26 @@ public class GetRequest implements Request {
 		int targetServerIndex = memcachedSocketHandler.findTargetServer(key);
 		
 		// TODO Send getrequest to designated server
+		
 		try {
 			memcachedSocketHandler.sendToSingleServer(readBuffer, targetServerIndex);
 			readBuffer.clear();
-			
+		} catch (IOException ex) {
+			logger.error(String.format("%s encountered an error when sending GetRequest to memcached server: %s", Thread.currentThread().getName(), ex.getMessage()));
+			logger.catching(ex);
+			RunMW.shutdown();
+		} finally {
+			readBuffer.clear();
+		}
+		
+		ByteBuffer response = null;
+		try {
 			// TODO read from designated server
-			ByteBuffer response = memcachedSocketHandler.waitForSingleResponse(targetServerIndex);
+			response = memcachedSocketHandler.waitForSingleResponse(targetServerIndex);
 			
 			// TODO Forward answerbuffer to client
 			sendResponseToClient(client, response);
-			response.clear();
+			
 		} catch (IOException ex) {
 			logger.error(String.format("%s couldn't handle GetRequest. Will close client connection: %s", Thread.currentThread().getName(), ex.getMessage()));
 			try {
@@ -56,7 +67,9 @@ public class GetRequest implements Request {
 				// Nothing we can do here
 				logger.catching(ex2);
 			}
-		}		
+		} finally {
+			if(response != null) response.clear();
+		}
 	}
 
 	private void sendResponseToClient(SocketChannel client, ByteBuffer response) throws IOException {

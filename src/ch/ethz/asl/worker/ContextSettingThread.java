e6d838521fb7a1381728e506cd22967a9d7945e7
Florian Chlan <florian@chlan.net>
2017-10-25 20:29:10 +0200

    Removed obsolete log message from ContextSettingThread

--- src/ch/ethz/asl/worker/ContextSettingThread.java
+++ src/ch/ethz/asl/worker/ContextSettingThread.java
@@ -1,12 +1,9 @@
 package ch.ethz.asl.worker;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 
 public class ContextSettingThread extends Thread {
 
-	private static final Logger logger = LogManager.getLogger(ContextSettingThread.class);
 	public ContextSettingThread(Runnable r) {
 		super(r);
 	}
@@ -14,7 +11,6 @@ public class ContextSettingThread extends Thread {
 	@Override
 	public void run() {
 		ThreadContext.put("KEY", Thread.currentThread().getName());
-		logger.debug("Hit Thread context initialization");
 		super.run();
 	}
 

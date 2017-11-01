a40650eee3e1cfe5b9cdc6ca76866e944ab63ab5
Florian Chlan <florian@chlan.net>
2017-11-01 12:54:18 +0100

    Continued implementation of first baseline script, Work In Progress!

--- bash/2_1_baseline_oneserver/experiment_master_script.sh
+++ bash/2_1_baseline_oneserver/experiment_master_script.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+# Functions
+create_dns_name() {
+	echo $nethz"@"$nethz$vm_nameprefix$1$vm_dns_suffix
+}
+create_vm_ip() {
+	echo "10.0.0."$1
+}
+
 # Parameters
+vm_nameprefix=foraslvms
+vm_dns_suffix=.westeurope.cloudapp.azure.com
+nethz=fchlan
+memcached_port=11211
+
+all_vms=(1 2 3 6)
+clients=(1 2 3)
+middlewares=()
+servers=(6)
+
 experiment=2_1_baseline_oneserver 
 
 num_repetitions=3
 params_vc_per_thread=(1 4 8 12 16 20 24 28 32)
+params_workload=(readOnly writeOnly)
+
 
 # Setup folder structure
 timestamp=$(date +%Y-%m-%d_%H%M%S)
@@ -16,6 +36,7 @@ cd $folder_name
 echo "Waiting for VMs to boot up"
 sleep 60
 
+memcached_cmd="memcached -p "$memcached_port" 2>&1 &"
 
 # For each repetition
 for $rep in $(seq $num_repetitions)
@@ -25,10 +46,23 @@ do
 	for vc_per_thread in ${params_vc_per_thread[@]}
 	do
 		echo "Starting experiment with vc_per_thread="$vc_per_thread
-# TODO 			Start memcached, wait shortly
-# TODO 			Start middleware, wait shortly
-# TODO 			Start memtiers
+		for workload in ${params_workload[@]}
+		do
+			echo "Starting experiment with" $workload "workload"
+ 			# Start memcached, wait shortly
+ 			for $mc_id in ${servers[@]}
+ 			do
+ 				ssh $(create_dns_name $servers) $memcached_cmd
+ 			done
+			sleep 4
+			echo "Started memcached servers"
 
+			# Start memtiers
+			ratio=#TODO
+			num_clients=#TODO
+			num_threads=#TODO
+			# Figure out correct command to use by reading the project description
+			memtier_cmd = "memtier_benchmark -s "$(create_vm_ip $servers)" -p "$memcached_port" -P memcache_text --run-count=1 --requests=1000000000 --clients="$num_clients" --threads="$num_threads" --expiry-range=9999-10000 --random-data --data-size-range=1-1024 --ratio="$ratio
 # TODO 			Wait for experiment duration
 
 # TODO 			Shutdown memtiers (make sure stats are printed, even in case of error or non-completion)
@@ -38,6 +72,7 @@ do
 # TODO 			Copy over logs from memtiers
 # TODO 			Copy over logs from middleware
 # TODO 			Put logs into right folder structure and rename
+		done
 	done
 done
 

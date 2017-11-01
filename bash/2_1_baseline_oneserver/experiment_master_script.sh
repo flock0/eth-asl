bf8d8f888b7a8acf4d251645e44e8d9c92e2212e
Florian Chlan <florian@chlan.net>
2017-11-01 08:36:59 +0100

    Created outline for experiment script

---
+++ bash/2_1_baseline_oneserver/experiment_master_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# TODO Setup folder structure
+# TODO	For each repetition
+# TODO 		For each parameter repetition
+# TODO 			Start memcached, wait shortly
+# TODO 			Start middleware, wait shortly
+# TODO 			Start memtiers
+
+# TODO 			Wait for experiment duration
+
+# TODO 			Shutdown memtiers (make sure stats are printed, even in case of error or non-completion)
+# TODO 			Shutdown Middleware
+# TODO 			Shutdown memcached
+
+# TODO 			Copy over logs from memtiers
+# TODO 			Copy over logs from middleware
+# TODO 			Put logs into right folder structure and rename
+# TODO 			Zip em up
+# TODO 			git commit
+# TODO 			push
---
+++ bash/bootstrap_master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+sudo apt-get update -y
+sudo apt-get install git unzip -y
+if [ -d "./ethz-asl-experiments" ]; then
+	cd ethz-asl-experiments/
+	git pull
+	cd
+else
+	git clone git@gitlab.ethz.ch:fchlan/ethz-asl-experiments.git
+fi

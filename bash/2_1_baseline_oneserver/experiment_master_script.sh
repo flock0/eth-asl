7abeb183b09dc3c0c00ea7f6ff8e7fe091b1abb2
Florian Chlan <florian@chlan.net>
2017-11-01 09:05:52 +0100

    Working on 2_1 experiment master script; Work in progress

--- bash/2_1_baseline_oneserver/experiment_master_script.sh
+++ bash/2_1_baseline_oneserver/experiment_master_script.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-# TODO Setup folder structure
-# TODO	For each repetition
-# TODO 		For each parameter repetition
+# Parameters
+experiment=2_1_baseline_oneserver 
+
+num_repetitions=3
+params_vc_per_thread=(1 4 8 12 16 20 24 28 32)
+
+# Setup folder structure
+timestamp=$(date +%Y-%m-%d_%H%M%S)
+folder_name=$experiment"_"$timestamp
+mkdir $folder_name
+cd $folder_name
+
+# Wait some time for all VMs to boot
+echo "Waiting for VMs to boot up"
+sleep 60
+
+
+# For each repetition
+for $rep in $(seq $num_repetitions)
+do
+	echo "Starting repetition" $rep
+	# For each parameter setting
+	for vc_per_thread in ${params_vc_per_thread[@]}
+	do
+		echo "Starting experiment with vc_per_thread="$vc_per_thread
 # TODO 			Start memcached, wait shortly
 # TODO 			Start middleware, wait shortly
 # TODO 			Start memtiers
@@ -16,6 +38,9 @@
 # TODO 			Copy over logs from memtiers
 # TODO 			Copy over logs from middleware
 # TODO 			Put logs into right folder structure and rename
+	done
+done
+
 # TODO 			Zip em up
 # TODO 			git commit
 # TODO 			push

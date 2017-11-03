d5d2b473deb43be4d1e32e459dad5a7a1642baaa
Florian Chlan <florian@chlan.net>
2017-11-03 10:59:32 +0100

    Add azure CLI to master

--- bash/bootstrap_master.sh
+++ bash/bootstrap_master.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get install apt-transport-https -y
 sudo apt-get update -y
-sudo apt-get install git unzip -y
+sudo apt-get install git unzip azure-cli -y
 if [ -d "./ethz-asl-experiments" ]; then
 	cd ethz-asl-experiments/
 	git pull

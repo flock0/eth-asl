9c6ae54eb8bc30fc66eeb35e37ad7f6b1c9b6cba
Florian Chlan <florian@chlan.net>
2017-11-12 20:46:13 +0100

    exp 3_1: Fixed folder of dstat.log; Redirected output of middleware-ssh command to /dev/null

--- bash/3_middleware_baseline/experiment_master_script.sh
+++ bash/3_middleware_baseline/experiment_master_script.sh
@@ -175,9 +175,9 @@ do
 
 			    for mw_id in ${middlewares[@]}
 				do
-					middleware_cmd="cd asl-fall17-project; > dstat.log; nohup dstat -cdlmnyt --output dstat.log 5 > /dev/null &
+					middleware_cmd="> dstat.log; nohup dstat -cdlmnyt --output dstat.log 5 > /dev/null & cd asl-fall17-project;
 			                        nohup java -jar bin/jar/ASL17_Middleware.jar -l "$(create_vm_ip $mw_id)" -p "$middleware_port" -t 2 
-			                        -s false -m "$(create_vm_ip ${servers[0]})":"$memcached_port" &"
+			                        -s false -m "$(create_vm_ip ${servers[0]})":"$memcached_port" > /dev/null &"
 					ssh $(create_vm_ip $mw_id) $middleware_cmd
 				done
 				sleep 4

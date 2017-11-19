c5be4640ff675ae2568bbae6b6f1e91ff325bb0d
Florian Chlan <florian@chlan.net>
2017-11-19 20:31:02 +0100

    Exp3 postprocessing: Added function for creating rolling window statistics

---
+++ bash/3_middleware_baseline/postprocessing/create_window_statistics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def create_window_statistics(metrics, windowsize):
+    metrics['timestamp'] = pd.to_datetime(metrics['initializeClockTime'], unit='ms')
+
+    mean_data = metrics.loc[:, ['timestamp', 'queueLength', 'responseTime_ms', 'queueTime_ms', 'workerServiceTime_ms']]
+    count_data = metrics.loc[:, ['timestamp']]
+    count_data['count'] = 1
+
+    mean_window = mean_data.rolling(windowsize, on='timestamp').mean()
+    count_window = count_data.rolling(windowsize, on='timestamp').count().drop('timestamp', axis=1)
+
+    return pd.concat([mean_window,count_window], axis=1)

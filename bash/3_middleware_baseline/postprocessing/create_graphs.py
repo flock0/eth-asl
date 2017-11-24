899c4178ce6142315fa08fba678b2d1544331eed
Florian Chlan <florian@chlan.net>
2017-11-24 17:47:50 +0100

    Exp 3 postprocessing: Renamed graph functions for xputresp vs. vc

--- bash/3_middleware_baseline/postprocessing/create_graphs.py
+++ bash/3_middleware_baseline/postprocessing/create_graphs.py
@@ -1,7 +1,7 @@
-import graph_functions as gf
+import graph_xputresp_vs_vc as xput_vs_vc
 
 # Experiment 3.1
-"""
+
 inputdir = "/home/flo/Downloads/exp3/third_exec/3_1_middleware_baseline_onemw_2017-11-23_182327"
 experiment_label = "exp3_1"
 
@@ -12,9 +12,9 @@ warmup_period_endtime = 10
 cooldown_period_starttime = 72
 outdir = "./graphs"
 num_threads = 2
-gf.create_workload_graphs(inputdir, experiment_label, "writeOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
-gf.create_workload_graphs(inputdir, experiment_label, "readOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
-"""
+xput_vs_vc.create_workload_graphs(inputdir, experiment_label, "writeOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
+xput_vs_vc.create_workload_graphs(inputdir, experiment_label, "readOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
+
 # Experiment 3.2
 
 inputdir = "/home/flo/Downloads/exp3/third_exec/3_2_middleware_baseline_twomws_2017-11-23_234338"
@@ -27,5 +27,5 @@ warmup_period_endtime = 10
 cooldown_period_starttime = 72
 outdir = "./graphs"
 num_threads = 2
-gf.create_workload_graphs(inputdir, experiment_label, "writeOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
-gf.create_workload_graphs(inputdir, experiment_label, "readOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
+xput_vs_vc.create_workload_graphs(inputdir, experiment_label, "writeOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)
+xput_vs_vc.create_workload_graphs(inputdir, experiment_label, "readOnly", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads)

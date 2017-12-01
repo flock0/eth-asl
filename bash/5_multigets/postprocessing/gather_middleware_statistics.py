24b25e89ca1d02c7794c1094848b6a44ad4dd020
Florian Chlan <florian@chlan.net>
2017-12-01 19:18:29 +0100

    Exp 5: Added xput and resp time graph

--- bash/5_multigets/postprocessing/gather_middleware_statistics.py
+++ bash/5_multigets/postprocessing/gather_middleware_statistics.py
@@ -35,9 +35,8 @@ def extract_metrics(requests):
     metrics['queueTime_ms'] = (requests['dequeueTime'] - requests['enqueueTime']) / 1000000
     metrics['workerServiceTime_ms'] = (requests['completedTime'] - requests['dequeueTime']) / 1000000
     metrics['memcachedRTT_ms'] = (requests['afterReceiveTime'] - requests['beforeSendTime']) / 1000000
-    metrics['netthreadServiceTime_ms'] = (requests['enqueueTime'] - requests['firstReadTime']) / 1000000
-    metrics['readTime_ms'] = (requests['arrivalTime'] - requests['firstReadTime']) / 1000000
-    metrics['responseTime_us'] = (requests['completedTime'] - requests['firstReadTime']) / 1000
+    metrics['netthreadServiceTime_ms'] = (requests['enqueueTime'] - requests['arrivalTime']) / 1000000
+    metrics['responseTime_us'] = (requests['completedTime'] - requests['arrivalTime']) / 1000
     metrics['responseTime_ms'] = metrics['responseTime_us'] / 1000
     metrics['timestep'] = (metrics['initializeClockTime'] - metrics['initializeClockTime'].min()) / 1000
     metrics = metrics.set_index('timestep')
--- bash/5_multigets/postprocessing/graph_xputresp_vs_multigets.py
+++ bash/5_multigets/postprocessing/graph_xputresp_vs_multigets.py
@@ -28,26 +28,19 @@ def create_workload_graphs(inputdir, experiment_label,  sharded, middlewares, cl
 
 def loop_function(experiment_label, all_directories, num_repetitions, middlewares, client_logfiles, warmup_period_endtime, cooldown_period_starttime, sharded, outdir, num_threads, ylim_xput, ylim_resp, xlim):
 
-    requestTypeName = ""
-    if sharded == "sharded":
-        requestTypeName = "ShardedGET"
-    elif sharded == "nonsharded":
-        requestTypeName = "NonshardedGET"
-
-    averages = get_data(all_directories, num_repetitions, middlewares, client_logfiles, warmup_period_endtime, cooldown_period_starttime, num_threads, requestTypeName)
-
+    averages = get_data(all_directories, num_repetitions, middlewares, client_logfiles, warmup_period_endtime, cooldown_period_starttime, num_threads)
+    averages = averages.sort_values('multigetSize')
 
     # Calculate interactive law values
     avg = averages.reset_index()
 
     #avg = avg.set_index(['workers', 'num_clients', 'index'])
 
-    plot_mw_xput_respTime_all_workers(avg, experiment_label, workload, outdir, ylim_xput, ylim_resp, xlim)
-    plot_mt_xput_respTime_all_workers_wInteract(avg, experiment_label, workload, outdir, ylim_xput, ylim_resp, xlim)
-    plot_mw_queueLength_all_workers(avg, experiment_label, workload, outdir, xlim)
-    plot_mw_memcachedRTT_all_workers(avg, experiment_label, workload, outdir, xlim)
+    plot_mw_xput_respTime_all_workers(avg, experiment_label, sharded, outdir, ylim_xput, ylim_resp, xlim)
+    #plot_mw_queueLength_all_workers(avg, experiment_label, sharded, outdir, xlim)
+    #plot_mw_memcachedRTT_all_workers(avg, experiment_label, sharded, outdir, xlim)
 
-def get_data(matching_dirs, num_repetitions, middlewares, client_logfiles, warmup_period_endtime, cooldown_period_starttime, num_threads, requestTypeName):
+def get_data(matching_dirs, num_repetitions, middlewares, client_logfiles, warmup_period_endtime, cooldown_period_starttime, num_threads):
     all_metrics_per_multiget = []
     for experiment_dir in matching_dirs:
         num_multiget = find_num_multigets(experiment_dir)
@@ -58,7 +51,8 @@ def get_data(matching_dirs, num_repetitions, middlewares, client_logfiles, warmu
             middleware_dirs = [get_only_subdir(os.path.join(experiment_dir, str(rep), mw_dir)) for mw_dir in middlewares]
             concatenated_requests = [gmws.concatenate_requestlogs(middleware_dir) for middleware_dir in middleware_dirs]
 
-            filtered_requests = [reqs[reqs['requestType']== requestTypeName] for reqs in concatenated_requests]
+
+            filtered_requests = [reqs[reqs['requestType'].str.contains("GET")] for reqs in concatenated_requests]
             metrics = [gmws.extract_metrics(reqs) for reqs in filtered_requests]
 
 
@@ -93,92 +87,30 @@ def get_data(matching_dirs, num_repetitions, middlewares, client_logfiles, warmu
 
     return all_metrics
 
-def plot_mw_xput_respTime_all_workers(avg, experiment_label, workload, outdir, ylim_xput, ylim_resp, xlim):
+def plot_mw_xput_respTime_all_workers(avg, experiment_label, sharded, outdir, ylim_xput, ylim_resp, xlim):
 
-    color_cycler = cycler('color', ['#ccece6', '#66c2a4', '#238b45', '#00441b'])
     # Throughput using interactive laws
     fig, ax = plt.subplots()
+    ax2 = ax.twinx()
     ax.set_ylim([0, ylim_xput])
     ax.set_xlim([0, xlim])
-    ax.set_prop_cycle(color_cycler)
-    for key, grp in avg.groupby(['workers']):
-        mean_values = grp[grp['index'] == 'mean'].reset_index()
-        std_values = grp[grp['index'] == 'std'].reset_index()
-        ax.errorbar(mean_values['num_clients'], mean_values['throughput_mw'], yerr=std_values['throughput_mw'],
-                    label=key, marker='o', capsize=3)
-        plt.xticks(mean_values['num_clients'])
-        ax.legend(loc="best", fontsize="small")
-    ax.set_title("Throughput (MW) vs. Number of clients\nfor differing worker count in the MW")
-    ax.set_xlabel("Number of clients")
-    ax.set_ylabel("Throughput (ops/sec)")
-
-    #plt.show()
-    fig.savefig(os.path.join(outdir, experiment_label, '{}_mw_throughput_{}.png'.format(workload, experiment_label)), dpi=300)
-
-    color_cycler = cycler('color', ['#fee391', '#fe9929', '#cc4c02', '#662506'])
-    fig, ax = plt.subplots()
-    ax.set_ylim([0, ylim_resp])
-    ax.set_xlim([0, xlim])
-    ax.set_prop_cycle(color_cycler)
-    for key, grp in avg.groupby(['workers']):
-        mean_values = grp[grp['index'] == 'mean'].reset_index()
-        std_values = grp[grp['index'] == 'std'].reset_index()
-        ax.errorbar(mean_values['num_clients'], mean_values['responseTime_ms'], yerr=std_values['responseTime_ms'],
-                    label=key, marker='o', capsize=3)
-        plt.xticks(mean_values['num_clients'])
-    ax.legend(loc="best", fontsize="small")
-    ax.set_title("Response Time (MW) vs. Number of clients\nfor differing worker count in the MW")
-    ax.set_xlabel("Number of clients")
-    ax.set_ylabel("Response Time (msec)")
+    ax2.set_ylim([0, ylim_resp])
+    ax2.set_xlim([0, xlim])
+    ax2.axvspan(74, 76, facecolor='#cc4c02', alpha=1)
+    ax.set_prop_cycle(cycler('color', ['#238b45']))
+    ax2.set_prop_cycle(cycler('color', ['#cc4c02']))
+    mean_values = avg[avg['index'] == 'mean'].reset_index()
+    std_values = avg[avg['index'] == 'std'].reset_index()
+    ax.errorbar(mean_values['multigetSize'], mean_values['throughput'], yerr=std_values['throughput'], label="Throughput", marker='o', capsize=3)
+    ax2.errorbar(mean_values['multigetSize'], mean_values['responsetime'], yerr=std_values['responsetime'], label="Response Time", marker='o', capsize=3)
+    plt.xticks(mean_values['multigetSize'])
+    ax.set_title("Throughput, Response Time (MT) vs. MultiGet sizes")
+    ax.set_xlabel("Sizes of MultiGets")
+    ax.set_ylabel("Throughput (op/s)", color='#238b45')
+    ax2.set_ylabel("Response Time (ms)", color='#cc4c02')
 
     # plt.show()
-    fig.savefig(os.path.join(outdir, experiment_label, '{}_mw_responsetime_{}.png'.format(workload, experiment_label)), dpi=300)
-
-def plot_mt_xput_respTime_all_workers_wInteract(avg, experiment_label, workload, outdir, ylim_xput, ylim_resp, xlim):
-
-    color_cycler = cycler('color', ['#ccece6', '#ccece6', '#66c2a4', '#66c2a4', '#238b45', '#238b45', '#00441b', '#00441b'])
-    # Throughput using interactive laws
-    fig, ax = plt.subplots()
-    ax.set_ylim([0, ylim_xput])
-    ax.set_xlim([0, xlim])
-    ax.set_prop_cycle(color_cycler)
-    for key, grp in avg.groupby(['workers']):
-        mean_values = grp[grp['index'] == 'mean'].reset_index()
-        std_values = grp[grp['index'] == 'std'].reset_index()
-        ax.errorbar(mean_values['num_clients'], mean_values['throughput'],
-                    yerr=std_values['throughput'],
-                    label=key, marker='o', capsize=3)
-        ax.plot(mean_values['num_clients'], mean_values['interact_throughput'], label="{} (i)".format(key), marker='o',
-                linestyle='--')
-        plt.xticks(mean_values['num_clients'])
-        ax.legend(loc="best", fontsize="small")
-    ax.set_title("Throughput (MT) vs. Number of clients\nfor differing worker count in the MW")
-    ax.set_xlabel("Number of clients")
-    ax.set_ylabel("Throughput (ops/sec)")
-
-    # plt.show()
-    fig.savefig(os.path.join(outdir, experiment_label, '{}_mt_throughput_{}.png'.format(workload, experiment_label)), dpi=300)
-
-    color_cycler = cycler('color', ['#fee391', '#fee391', '#fe9929', '#fe9929', '#cc4c02', '#cc4c02', '#662506', '#662506'])
-    fig, ax = plt.subplots()
-    ax.set_ylim([0, ylim_resp])
-    ax.set_xlim([0, xlim])
-    ax.set_prop_cycle(color_cycler)
-    for key, grp in avg.groupby(['workers']):
-        mean_values = grp[grp['index'] == 'mean'].reset_index()
-        std_values = grp[grp['index'] == 'std'].reset_index()
-        ax.errorbar(mean_values['num_clients'],  mean_values['responsetime'], yerr=std_values['responsetime'],
-                    label=key, marker='o', capsize=3)
-        ax.plot(mean_values['num_clients'], mean_values['interact_resptime'], label="{} (i)".format(key), marker='o',
-                linestyle='--')
-        plt.xticks(mean_values['num_clients'])
-    ax.legend(loc="best", fontsize="small")
-    ax.set_title("Response Time (MT) vs. Number of clients\nfor differing worker count in the MW")
-    ax.set_xlabel("Number of clients")
-    ax.set_ylabel("Response Time (msec)")
-
-    # plt.show()
-    fig.savefig(os.path.join(outdir, experiment_label, '{}_mt_responsetime_{}.png'.format(workload, experiment_label)), dpi=300)
+    fig.savefig(os.path.join(outdir, experiment_label, '{}_mw_xput_resptime_{}.png'.format(sharded, experiment_label)), dpi=300)
 
 def plot_mw_queueLength_all_workers(avg, experiment_label, workload, outdir, xlim):
 
@@ -229,26 +161,3 @@ def plot_mw_memcachedRTT_all_workers(avg, experiment_label, workload, outdir, xl
         os.path.join(outdir, experiment_label, '{}_mw_memcachedRTT_{}.png'.format(workload, experiment_label)),
         dpi=300)
 
-
-
-import gather_memtier_statistics as gmts
-import matplotlib.pyplot as plt
-import os, sys
-exp5_dir = "/home/flo/Documents/eth-asl-final-experiment-data/exp5/5_1_multigets_sharded_2017-11-24_085912"
-# Experiment 4.2: Full setup, write only
-# Plot the throughput and response time measured in the middleware
-
-inputdir = exp5_dir
-experiment_label = "exp4"
-
-middlewares = ["middleware_04", "middleware_05"]
-client_logfiles = ["client_01_0.log", "client_01_1.log", "client_02_0.log", "client_02_1.log", "client_03_0.log", "client_03_1.log"]
-dir_suffix_regex_string = "_\d*multiget"
-warmup_period_endtime = 10
-cooldown_period_starttime = 72
-outdir = "./graphs"
-num_threads = 6
-ylim_xput = 30000
-ylim_resp = 60
-xlim = 420
-create_workload_graphs(inputdir, experiment_label, "sharded", middlewares, client_logfiles, dir_suffix_regex_string, warmup_period_endtime, cooldown_period_starttime, outdir, num_threads, ylim_xput, ylim_resp, xlim)

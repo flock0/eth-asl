b24a8304a45554b8808ad646a280d3170fcb1f15
Florian Chlan <florian@chlan.net>
2017-12-07 11:12:07 +0100

    Exp 2,3,4,5 postprocessing: Updated some graphs

--- bash/5_multigets/postprocessing/graph_responsetime.py
+++ bash/5_multigets/postprocessing/graph_responsetime.py
@@ -118,3 +118,126 @@ def graph_responsetime(multigets, sharded, middlewares, client_logfiles, reps, i
     ax.set_xlabel("Time (msec)")
     ax.set_xlim([0, xlim])
 
+def get_responsetime_data(sharded, multigets, middlewares, client_logfiles, reps, inputdir):
+
+    all_metrics_per_multiget = []
+    for multiget in multigets:
+        all_mw_metrics_per_rep = []
+        all_mt_metrics_per_rep = []
+        for rep in reps:
+            run_dir = os.path.join(inputdir, "{}_{}multiget".format(sharded, multiget), str(rep))
+
+            # Get MW response time
+            middleware_dirs = [dirfuncs.get_only_subdir(os.path.join(run_dir, mw_dir)) for mw_dir in middlewares]
+            concatenated_requests = [gmws.concatenate_requestlogs(middleware_dir) for middleware_dir in middleware_dirs]
+
+            metrics = [gmws.extract_metrics(reqs) for reqs in concatenated_requests]
+
+            cut_metrics = [cut.cut_away_warmup_cooldown(mets, 10, 72) for mets in
+                           metrics]
+
+            windows = [gmws.aggregate_over_windows(cut_mets) for cut_mets in cut_metrics]
+
+            rep_metrics = gmws.aggregate_over_middlewares(windows)
+
+            all_mw_metrics_per_rep.append(rep_metrics)
+
+            # Get MT response time
+            client_logfile_paths = [os.path.join(run_dir, client_logfile) for client_logfile in
+                                    client_logfiles]
+            client_metrics = gmts.aggregate_over_clients(client_logfile_paths)
+
+            cut_client_metrics = cut.cut_away_warmup_cooldown(client_metrics, 10, 72)
+
+            all_mt_metrics_per_rep.append(cut_client_metrics)
+
+        mw_agg_over_reps = gmws.aggregate_over_reps(all_mw_metrics_per_rep)
+        mw_averages = gmws.aggregate_over_timesteps(mw_agg_over_reps)
+        mt_agg_over_reps = gmts.aggregate_over_reps(all_mt_metrics_per_rep)
+        mt_averages = gmts.aggregate_over_timesteps(mt_agg_over_reps)
+
+        metrics = pd.concat([mw_averages, mt_averages], axis=1)
+        metrics['multigets'] = multiget
+        all_metrics_per_multiget.append(metrics)
+
+    all_metrics = pd.concat(all_metrics_per_multiget)
+    return all_metrics.loc[
+        'mean', :]
+
+def get_responsetime_data_old(sharded, multigets, middlewares, client_logfiles, reps, inputdir):
+
+    all_metrics_per_multiget = []
+    for multiget in multigets:
+        all_mw_metrics_per_rep = []
+        all_mt_metrics_per_rep = []
+        for rep in reps:
+            run_dir = os.path.join(inputdir, "{}_{}multiget".format(sharded, multiget), str(rep))
+
+            # Get MW response time
+            middleware_dirs = [dirfuncs.get_only_subdir(os.path.join(run_dir, mw_dir)) for mw_dir in middlewares]
+            concatenated_requests = [gmws.concatenate_requestlogs(middleware_dir) for middleware_dir in middleware_dirs]
+
+            metrics = [gmws.extract_metrics_old(reqs) for reqs in concatenated_requests]
+
+            cut_metrics = [cut.cut_away_warmup_cooldown(mets, 10, 72) for mets in
+                           metrics]
+
+            windows = [gmws.aggregate_over_windows(cut_mets) for cut_mets in cut_metrics]
+
+            rep_metrics = gmws.aggregate_over_middlewares(windows)
+
+            all_mw_metrics_per_rep.append(rep_metrics)
+
+            # Get MT response time
+            client_logfile_paths = [os.path.join(run_dir, client_logfile) for client_logfile in
+                                    client_logfiles]
+            client_metrics = gmts.aggregate_over_clients(client_logfile_paths)
+
+            cut_client_metrics = cut.cut_away_warmup_cooldown(client_metrics, 10, 72)
+
+            all_mt_metrics_per_rep.append(cut_client_metrics)
+
+        mw_agg_over_reps = gmws.aggregate_over_reps(all_mw_metrics_per_rep)
+        mw_averages = gmws.aggregate_over_timesteps(mw_agg_over_reps)
+        mt_agg_over_reps = gmts.aggregate_over_reps(all_mt_metrics_per_rep)
+        mt_averages = gmts.aggregate_over_timesteps(mt_agg_over_reps)
+
+        metrics = pd.concat([mw_averages, mt_averages], axis=1)
+        metrics['multigets'] = multiget
+        all_metrics_per_multiget.append(metrics)
+
+    all_metrics = pd.concat(all_metrics_per_multiget)
+    return all_metrics.loc[
+        'mean', :]
+
+def plot_stacked_barchart(data, xtick_labels, worker, ax, ylim, print_first_n_bars):
+    data = data.sort_values('multigets')
+    data = data.head(print_first_n_bars)
+    netthread = data['netthreadServiceTime_ms']
+    queuetime = data['queueTime_ms']
+    workerPreProcessing = data['workerPreProcessingTime_ms']
+    memcachedRTT = data['memcachedRTT_ms']
+    workerPostProcessing = data['workerPostProcessingTime_ms']
+    memtierResponseTime = data['responsetime']
+    x = [1, 1.5, 2, 2.5]
+    x = x[:print_first_n_bars]
+    width = 0.35
+    memtierResponseTimeBar = ax.bar(x=x, width=width, height=memtierResponseTime, color='#fbb4ae')
+    netthreadBar = ax.bar(x=x, width=width, height=netthread, color='#bc80bd')
+    queuetimeBar = ax.bar(x=x, width=width, height=queuetime, bottom=netthread, color='#b3de69')
+    workerPreBar = ax.bar(x=x, width=width, height=workerPreProcessing, bottom=netthread + queuetime, color='#fdb462')
+    memcachedRTTBar = ax.bar(x=x, width=width, height=memcachedRTT, bottom=netthread + queuetime + workerPreProcessing,
+                             color='#80b1d3')
+    workerPostBar = ax.bar(x=x, width=width, height=workerPostProcessing,
+                           bottom=netthread + queuetime + workerPreProcessing + memcachedRTT, color='#fb8072')
+    ax.legend((netthreadBar[0], queuetimeBar[0], workerPreBar[0], memcachedRTTBar[0], workerPostBar[0],
+               memtierResponseTimeBar[0]),
+              ('Network Thread', 'Queue Time', 'Worker Preprocessing', 'Memcached RTT', 'Worker Postprocessing',
+               'Latency MT<->MW'),
+              loc='upper left')
+    ax.set_title("{} workers".format(worker))
+    ax.set_xlabel("Number of clients")
+    ax.set_ylabel("Response Time (ms)")
+    ax.set_ylim([0, ylim])
+    #plt.xticks(x)
+    ax.set_xticklabels(['dummy'] + xtick_labels[:print_first_n_bars])
